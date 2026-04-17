c88d6543cc676ee7f01f014322759b91b1d23d53
konnosr <konnosr123@gmail.com>
2026-04-17 13:02:15 +0300

    Addeed the progress bar and got it to work correctly!

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
-import rich
+from rich.progress import Progress, BarColumn, TextColumn
+from rich.console import Console
 import os
 
 #clear the terminal
@@ -17,17 +18,39 @@ def format_time(milliseconds):
 #main programme
 def main():
     clear_screen()
+
+    file_path = "test.flac"
+
     pygame.mixer.init()
-    pygame.mixer.music.load("test.flac")
+    pygame.mixer.music.load(file_path)
     pygame.mixer.music.play()
 
-    total_length = int(pygame.mixer.Sound("test.flac").get_length()*1000)
+    total_length = int(pygame.mixer.Sound(file_path).get_length()*1000)
     print(format_time(total_length))
 
-    #while pygame.mixer.music.get_busy():
-    #    current_time = pygame.mixer.music.get_pos()
-    #    print(current_time)
-        #clear_screen()
+
+    with Progress(
+            TextColumn("{task.description}[/]", justify="right"),
+            BarColumn(bar_width=None),
+            TextColumn("{task.fields[suffix]}", justify="right"),
+        ) as progress:
+
+        playback = progress.add_task(
+        f"[#03ad00]Playing: [white]{file_path} [red]< [#00d0ff]",
+        total=total_length, 
+        suffix="[#00d0ff] [red]>")
+
+        while pygame.mixer.music.get_busy(): #music starts
+            current_time = pygame.mixer.music.get_pos()
+
+            progress.update(playback, 
+                            completed=current_time,
+                            description=f"[#03ad00]Playing: [white]{file_path} [red]< [#00d0ff]{format_time(current_time)}",
+                            suffix=f"[#00d0ff]{format_time(total_length - current_time)} [red]>"
+                            )
+
+
+
 
 
 if __name__ == "__main__":
